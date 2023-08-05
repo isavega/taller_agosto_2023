641d405c1d53a60d53619cdc8eef5455ae161e38
Juan Carlos Galue <juancgalue@icloud.com>
2023-08-05 12:45:09 -0400

    style: :lipstick: Ajusto el estilo para darle un min-height

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,25 +1,26 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
 
 interface ContainerProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 const CenteredContainer = styled.div`
   background-color: #000;
   opacity: 0.8;
   border-radius: 15px;
-  width: 80%;
+  width: 100%;
+  min-height: 100vh;
   height: 80%;
   display: flex;
-  justify-content: center;
+  justify-content: start;
   align-items: center;
   flex-direction: column;
   margin: auto; /* Agrega margen automático para centrar el contenedor en la página */
-`;
+`
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
-  return <CenteredContainer>{children}</CenteredContainer>;
-};
+  return <CenteredContainer>{children}</CenteredContainer>
+}
 
-export default Container;
+export default Container
